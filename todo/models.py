1e5bb67ec28b325908c930ba4c0cea24f54edb74
Alex Sparrow <alexsilverhawk@gmail.com>
2020-08-30 12:00:34 -0700

    Add model for Todo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ToDo(models.Model):
+    # Content of the todo
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+
+    # Created at
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # User who owns this todo
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Urgency of this todo
+    urgent = models.BooleanField(default=False)
+
+    # Completed boolean
+    completed = models.DateTimeField(null=True)
